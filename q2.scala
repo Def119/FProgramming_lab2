91d07bd6830c834a39e80f69cd2af9024a7d3b48
Def119 <75200612+Def119@users.noreply.github.com>
2024-06-24 22:38:50 +0530

    Update q2.scala
    post increment edited

--- q2.scala
+++ q2.scala
@@ -5,13 +5,17 @@
     var d: Int = 5;
     var k: Float = 4.3f;
     b -= 1;
-    d -= 1;
+    
     println(s" - -b * a + c *d - - : ${ (b) * a + c *(d)} ");
-    a = a+1;
+    d -= 1;
+    
     println(s"a++ : ${a}");
+    a = a+1;
+
     println (s"-2 * ( g - k ) +c : ${-2 * ( a - k ) +c}");  //is it a or g?
+    
     c += 1;
-    println (s"c=c++ : $c");
+    println (s"c=c++ : $c");    //what is expected value of c after post incrementing>
     c += 1 ;
     c = c + a ;
     a += 1 ;
